4e78870d55864f0b3c00237e77aea2936b14701f
wandson <wandson@ivision.ind.br>
2017-03-30 13:31:23 -0300

    device's process that streams up images working

--- linux_app/image-feeder/image-feeder.c
+++ linux_app/image-feeder/image-feeder.c
@@ -1,7 +1,8 @@
 
-#include <pthread>
-#include <stdio>
-#include <stdlib>
+#include <pthread.h>
+#include <semaphore.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <sys/mman.h>
 #include <unistd.h>
 #include <fcntl.h>
@@ -20,41 +21,47 @@
 void* frame_base;
 
 
+
+
+
 /******************  RING BUFFER ************************/
 
 
 #define SIZE_RINGBUFFER 4
 
+struct RINGBUFFER;
 
-typedef struct
+
+typedef struct RINGBUFFER
 {
-  void *pBuffer[SIZE_RINGBUFER];
-  unsigned intt indexInsert;
+  void *pBuffer[SIZE_RINGBUFFER];
+  unsigned int indexInsert;
   unsigned int indexRemove;
   unsigned int count;
-  p_thread_mutex_t mutex;
+  pthread_mutex_t mutex;
   sem_t semEmpty;
   sem_t semFull;
 
-  void (*f_addBuffer)(T_RINGBUFFER *ringBuffer, void *buffer);
-  void (*f_getBuffer)(T_RINGBUFFER *ringBuffer, void **buffer);
+  void (*f_addBuffer)(struct RINGBUFFER *ringBuffer, void *buffer);
+  void (*f_getBuffer)(struct RINGBUFFER *ringBuffer, void **buffer);
 } T_RINGBUFFER;
 
 
 void addBuffer(T_RINGBUFFER *ringBuffer, void *buffer)
 {
   sem_wait(&(ringBuffer->semFull));
-  p_thread_mutex_lock(&(ringBuffer->mutex));
-  if (ringBuffer->count < SIZE_RINGBUFER)
+  pthread_mutex_lock(&(ringBuffer->mutex));
+  if (ringBuffer->count < SIZE_RINGBUFFER)
     {
       ringBuffer->pBuffer[ringBuffer->indexInsert] = buffer;
-      if (++ringBuffer->indexInsert > SIZE_RINGBUFER)
+      if (++(ringBuffer->indexInsert) >= SIZE_RINGBUFFER)
 	{
 	  ringBuffer->indexInsert = 0;
 	}
       ringBuffer->count++;
     }
-  p_thread_mutex_unlock(&(ringBuffer->mutex));
+  //printf("added buffer %x at position %d. New count: %d\n", (unsigned int)buffer, ringBuffer->indexInsert, ringBuffer->count);
+  pthread_mutex_unlock(&(ringBuffer->mutex));
   sem_post(&(ringBuffer->semEmpty));
 }
 
@@ -62,17 +69,18 @@ void addBuffer(T_RINGBUFFER *ringBuffer, void *buffer)
 void getBuffer(T_RINGBUFFER *ringBuffer, void **buffer)
 {
   sem_wait(&(ringBuffer->semEmpty));
-  p_thread_mutex_lock(&(ringBuffer->mutex));
+  pthread_mutex_lock(&(ringBuffer->mutex));
   if (ringBuffer->count > 0)
     {
       *buffer = ringBuffer->pBuffer[ringBuffer->indexRemove];
-      if (++ringBuffer->indexRemove > SIZE_RINGBUFER)
+      if (++(ringBuffer->indexRemove) >= SIZE_RINGBUFFER)
 	{
 	  ringBuffer->indexRemove = 0;
 	}
       ringBuffer->count--;
     }
-  p_thread_mutex_unlock(&(ringBuffer->mutex));
+  //printf("retrieved buffer %x from position %d. New count: %d\n", (unsigned int)*buffer, ringBuffer->indexRemove, ringBuffer->count);
+  pthread_mutex_unlock(&(ringBuffer->mutex));
   sem_post(&(ringBuffer->semFull));
 }
 
@@ -81,43 +89,16 @@ void initRingBuffer(T_RINGBUFFER *ringBuffer)
   ringBuffer->count = 0;
   ringBuffer->indexInsert = 0;
   ringBuffer->indexRemove = 0;
-  f_addBuffer = addBuffer;
-  f_getBuffer = getBuffer;
+  ringBuffer->f_addBuffer = addBuffer;
+  ringBuffer->f_getBuffer = getBuffer;
   sem_init(&(ringBuffer->semFull), 0, SIZE_RINGBUFFER);
   sem_init(&(ringBuffer->semEmpty), 0, 0);
-  pthread_mutex_init(&(ringBuffer->mutex));
+  pthread_mutex_init(&(ringBuffer->mutex), NULL);
 }
 
-void initSocket(char* host)
-{
-  hp = gethostbyname(host);
-  if (!hp) {
-    fprintf(stderr, "socket: unknown host: %s\n", host);
-    exit(1);
-  }
-  /* build address data structure */
-  bzero((char *)&sin, sizeof(sin));
-  sin.sin_family = AF_INET;
-  bcopy(hp->h_addr, (char *)&sin.sin_addr, hp->h_length);
-  sin.sin_port = htons(SERVER_PORT);
-  /* active open */
-  if ((int_socket = socket(PF_INET, SOCK_STREAM, 0)) < 0) {
-    perror("socket: socket error");
-    exit(1);
-  }
-  if (connect(int_socket, (struct sockaddr *)&sin, sizeof(sin)) < 0)
-    {
-      perror("socket: connect error");
-      close(int_socket);
-      exit(1);
-    }
-
-  
-  
-  
-}
 
 /**********************  end ring buffer *********************************/
+
 
 
 /************************Global Variables*****************************/
@@ -127,10 +108,50 @@ void *frame_base;
 T_RINGBUFFER ringBufferIdle;
 T_RINGBUFFER ringBufferImagesToNet;
 void *buffers[SIZE_RINGBUFFER];
+int errOk;
 
 struct hostent *hp;
-struct sockaddr_in sin;
+struct sockaddr_in socket_in;
 int int_socket;
+
+
+
+
+
+void initSocket(char* ip)
+{
+  //printf("Iniciando conexao %s\n", ip);
+  hp = gethostbyname(ip);
+  if (!hp) {
+    //printf(stderr, "socket: unknown host: %s\n", ip);
+    exit(1);
+  }
+  //printf("Host ok\n"); 
+  /* build address data structure */
+  bzero((char *)&socket_in, sizeof(socket_in));
+  socket_in.sin_family = AF_INET;
+  bcopy(hp->h_addr, (char *)&socket_in.sin_addr, hp->h_length);
+  socket_in.sin_port = htons(SERVER_PORT);
+  /* active open */
+  if ((int_socket = socket(PF_INET, SOCK_STREAM, 0)) < 0) {
+    perror("socket: socket error");
+    exit(1);
+  }
+  //printf("SOCKET ok\n");
+  if (connect(int_socket, (struct sockaddr *)&socket_in, sizeof(socket_in)) < 0)
+    {
+      perror("socket: connect error");
+      close(int_socket);
+      exit(1);
+    }
+  //printf("Connection ok\n");
+
+  
+  
+  
+}
+
+
 
 
 
@@ -138,11 +159,13 @@ void f_bufferProducer()
 {
   void *imgBuffer;
 
-  while (1)
+  while (errOk)
     {
-      ringBufferIdle.getBuffer(&ringBufferidle, &imgBuffer);
-      memcpy(idleBuffer, frame_base, FRAME_SIZE);
-      ringBufferImagesToNet.addBuffer(&ringBufferImagesToNet, idleBuffer);
+      ringBufferIdle.f_getBuffer(&ringBufferIdle, &imgBuffer);
+      //printf("P: buffer %x retrieved from idle ring\n", (unsigned int)imgBuffer);
+      memcpy(imgBuffer, frame_base, FRAME_SIZE);
+      ringBufferImagesToNet.f_addBuffer(&ringBufferImagesToNet, imgBuffer);
+      //printf("P: buffer %x added to net ring\n", (unsigned int)imgBuffer);
     }
 }
 
@@ -151,12 +174,21 @@ void f_bufferConsumer()
 {
   void *netBuffer;
 
-  while(1)
+  while(errOk)
     {
-      ringBufferImagesToNet.getBuffer(&ringBufferImagesToNet, &netBuffer);
-      send(int_socket, netBuffer, FRAME_SIZE, 0);
-      //send socket
-      ringBufferIdle.addBuffer(&ringBufferIdle, netBuffer);
+      ringBufferImagesToNet.f_getBuffer(&ringBufferImagesToNet, &netBuffer);
+      //////printf("C: buffer %x retrieved from net ring\n", (unsigned int)netBuffer);
+      if (send(int_socket, netBuffer, FRAME_SIZE, 0) < 0)
+	{
+	  printf("error sending data ...\n");
+	  errOk = 0;
+	}
+      else
+	{
+	  //printf("image sent\n");
+	  ringBufferIdle.f_addBuffer(&ringBufferIdle, netBuffer);
+	}
+      //////printf("C: buffer %x returned to idle ring\n", (unsigned int)netBuffer);
     }
 }
 
@@ -164,44 +196,64 @@ void init()
 {
   int it;
   
-  fd = open("/dev/mem", (O_RDWR|O_SYNC));  
+  fd = open("/dev/mem", (O_RDWR|O_SYNC));
   fd_mem = (void *) malloc(2592*1944*sizeof(char));			  
   frame_base = mmap(NULL,FRAME_SPAN,(PROT_READ|PROT_WRITE),MAP_SHARED,fd,FRAME_ADDR);
+  ////printf("outside memory mapped\n");
   initRingBuffer(&ringBufferIdle);
+  ////printf("created idle ring buffer\n");
   initRingBuffer(&ringBufferImagesToNet);
+  ////printf("created net ring buffer\n");
 
+  errOk = 1;
+  
   for (it=0; it<SIZE_RINGBUFFER; ++it)
     {
       buffers[it] = (void *) malloc(FRAME_SIZE*sizeof(char));
-      ringBufferIdle.addBuffer(&ringBufferIdle, buffers[it]);
+      ////printf("memory allocated for buffer %d\n", it);
+      ringBufferIdle.f_addBuffer(&ringBufferIdle, buffers[it]);
+      ////printf ("buffer added to ring iddle\n");
     }
 }
 
-			  
-int main(int argv, char* argv[])
-{
 
+void terminate()
+{
+  int it;
+  for (it=0; it<SIZE_RINGBUFFER; ++it)
+    {
+      free(buffers[it]);
+    }
+   close(int_socket);
+}  
+
+			  
+int main(int argc, char* argv[])
+{
+  init();
   initSocket(argv[1]);
   pthread_t bufferProducer, bufferConsumer;
+  int err;
 
 
-  h_threadBufferProducer = pthread_create(&bufferProducer, NULL, f_bufferProducer, NULL);
-if (h_threadBufferPrioducer)
+  err = pthread_create(&bufferProducer, NULL, f_bufferProducer, NULL);
+  if (err)
   {
-    printf(stderr,"Error - pthread_create() return code: %d\n",h_threadBufferProducer);
+    ////printf(stderr,"Error - pthread_create() return code: %d\n",err);
     exit(EXIT_FAILURE);
   }
 
- h_threadBufferConsumer = pthread_create(&bufferConsumer, NULL, f_bufferConsumer, NULL);
-if (h_threadBufferConsumer)
+ err = pthread_create(&bufferConsumer, NULL, f_bufferConsumer, NULL);
+if (err)
   {
-    printf(stderr,"Error - pthread_create() return code: %d\n",h_threadBufferConsumer);
+    ////printf(stderr,"Error - pthread_create() return code: %d\n",err);
     exit(EXIT_FAILURE);
   }
 
- pthread_join(bufferProducer);
- pthread_join(bufferConsumer);
+ pthread_join(bufferProducer, NULL);
+ pthread_join(bufferConsumer, NULL);
 
+ terminate();
 }
 
 
