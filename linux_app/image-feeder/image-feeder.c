647d4de5d0d256c6a77b7564d2aa09866e7f2980
wandson <wandson@ivision.ind.br>
2017-03-31 22:22:50 -0300

    adding ping pong buffer in software

--- linux_app/image-feeder/image-feeder.c
+++ linux_app/image-feeder/image-feeder.c
@@ -12,11 +12,27 @@
 #include <netinet/in.h>
 #include <netdb.h>
 
-#define FRAME_ADDR 0x38000000
+#define SERVER_PORT 4444
+
+
+#define LW_AXI_BASE 0xFF200000 //ARM LW ADDR BASE
+#define LW_AXI_SPAN 0x00200000 //2 MB SIZE
+#define FPGA_FRAMEBUFFER_CONTROL_ADDRBASE  (0x10000)
+#define F2H_AXI_BASE 0xC0000000
+#define F2H_AXI_SPAN 0x80000
+
+#define FPGA_FRAMEBUFFER_REG_READIMAGEADDR 0
+#define FPGA_FRAMEBUFFER_REG_CONTROLSTATUS 1
+#define FPGA_FRAMEBUFFER_CONTROL_BIT_REQ_READBUFFER 1
+#define FPGA_FRAMEBUFFER_CONTROL_BIT_STATUS_READBUFFERAVAILABLE 2
+
+#define FPGA_FRAME0_ADDR 0x38000000
+#define FPGA_FRAME1_ADDR 0x38500000
 #define FRAME_SPAN 0x500000
 #define FRAME_SIZE 2592*1944
 
-#define SERVER_PORT 4444
+
+
 
 void* frame_base;
 
@@ -79,6 +95,10 @@ void getBuffer(T_RINGBUFFER *ringBuffer, void **buffer)
 	}
       ringBuffer->count--;
     }
+  else
+    {
+      *buffer = NULL;
+    }
   //printf("retrieved buffer %x from position %d. New count: %d\n", (unsigned int)*buffer, ringBuffer->indexRemove, ringBuffer->count);
   pthread_mutex_unlock(&(ringBuffer->mutex));
   sem_post(&(ringBuffer->semFull));
@@ -102,17 +122,26 @@ void initRingBuffer(T_RINGBUFFER *ringBuffer)
 
 
 /************************Global Variables*****************************/
-int fd;
-void *fd_mem;
-void *frame_base;
-T_RINGBUFFER ringBufferIdle;
-T_RINGBUFFER ringBufferImagesToNet;
-void *buffers[SIZE_RINGBUFFER];
-int errOk;
+static int fd;
+static void *fd_mem;
+static T_RINGBUFFER ringBufferIdle;
+static T_RINGBUFFER ringBufferImagesToNet;
+static void *buffers[SIZE_RINGBUFFER];
+static int errOk;
+
+static int fpga_lwfd;
+static void *fpga_lw_base;
+static void *fpga_frameBufferControl_baseAddr;
+
+static void *fpgaFrameBuffer[2];
+
+
 
 struct hostent *hp;
 struct sockaddr_in socket_in;
 int int_socket;
+
+
 
 
 
@@ -154,23 +183,52 @@ void initSocket(char* ip)
 
 
 
-
-void f_bufferProducer()
+static void* getFpgaFrameBuffer()
 {
-  void *imgBuffer;
+  printf("debug before bla\n");
+  *((uint32_t*)fpga_frameBufferControl_baseAddr + FPGA_FRAMEBUFFER_REG_CONTROLSTATUS) |= FPGA_FRAMEBUFFER_CONTROL_BIT_REQ_READBUFFER;
+  printf("debug\n");
+  printf("lw addr reg value: %x\n", *((uint32_t*)fpga_frameBufferControl_baseAddr));
+  printf("lw control reg value: %x\n", *((uint32_t*)fpga_frameBufferControl_baseAddr + FPGA_FRAMEBUFFER_REG_CONTROLSTATUS));
+  printf("lw control reg: %x\n", (uint32_t)(uint32_t*)(fpga_frameBufferControl_baseAddr + FPGA_FRAMEBUFFER_REG_CONTROLSTATUS));
+  printf("lw control bit: %x\n", (uint32_t)FPGA_FRAMEBUFFER_CONTROL_BIT_REQ_READBUFFER);
+  printf("lw control bit: %x\n", (uint32_t)FPGA_FRAMEBUFFER_CONTROL_BIT_STATUS_READBUFFERAVAILABLE);
+	 
+  while ( !( *((uint32_t*)fpga_frameBufferControl_baseAddr + FPGA_FRAMEBUFFER_REG_CONTROLSTATUS) & FPGA_FRAMEBUFFER_CONTROL_BIT_STATUS_READBUFFERAVAILABLE ) );
+  return fpgaFrameBuffer[*((uint32_t*)fpga_frameBufferControl_baseAddr + FPGA_FRAMEBUFFER_REG_READIMAGEADDR)];
+}
+
+static endBufferRead()
+{
+  *((uint32_t*)fpga_frameBufferControl_baseAddr + FPGA_FRAMEBUFFER_REG_CONTROLSTATUS) &= ~FPGA_FRAMEBUFFER_CONTROL_BIT_REQ_READBUFFER;
+}
+
+
+static void f_bufferProducer()
+{
+  void *imgBuffer, *fpgaFrame;
 
   while (errOk)
     {
       ringBufferIdle.f_getBuffer(&ringBufferIdle, &imgBuffer);
+      if (imgBuffer == NULL)
+	{
+	  errOk = 0;
+	}
+      else
+	{
+	  fpgaFrame = getFpgaFrameBuffer();
       //printf("P: buffer %x retrieved from idle ring\n", (unsigned int)imgBuffer);
-      memcpy(imgBuffer, frame_base, FRAME_SIZE);
-      ringBufferImagesToNet.f_addBuffer(&ringBufferImagesToNet, imgBuffer);
+	  memcpy(imgBuffer, fpgaFrame, FRAME_SIZE);
+	  endBufferRead();
+	  ringBufferImagesToNet.f_addBuffer(&ringBufferImagesToNet, imgBuffer);
+	}
       //printf("P: buffer %x added to net ring\n", (unsigned int)imgBuffer);
     }
 }
 
 
-void f_bufferConsumer()
+static void f_bufferConsumer()
 {
   void *netBuffer;
 
@@ -197,8 +255,17 @@ void init()
   int it;
   
   fd = open("/dev/mem", (O_RDWR|O_SYNC));
-  fd_mem = (void *) malloc(2592*1944*sizeof(char));			  
-  frame_base = mmap(NULL,FRAME_SPAN,(PROT_READ|PROT_WRITE),MAP_SHARED,fd,FRAME_ADDR);
+  fpgaFrameBuffer[0] = mmap(NULL, FRAME_SPAN, (PROT_READ|PROT_WRITE), MAP_SHARED, fd, FPGA_FRAME0_ADDR);
+  fpgaFrameBuffer[1] = mmap(NULL, FRAME_SPAN, (PROT_READ|PROT_WRITE), MAP_SHARED, fd, FPGA_FRAME1_ADDR);
+
+  printf("debug before\n");
+  fpga_lwfd = open("/dev/mem",(O_RDWR|O_SYNC));
+  printf("debug before2\n");
+  fpga_lw_base = mmap(NULL, F2H_AXI_SPAN, (PROT_READ|PROT_WRITE), MAP_SHARED, fpga_lwfd, LW_AXI_BASE);
+  printf("debug before3\n");
+  fpga_frameBufferControl_baseAddr = fpga_lw_base + FPGA_FRAMEBUFFER_CONTROL_ADDRBASE;
+  printf("debug before4\n");
+
   ////printf("outside memory mapped\n");
   initRingBuffer(&ringBufferIdle);
   ////printf("created idle ring buffer\n");
@@ -225,6 +292,8 @@ void terminate()
       free(buffers[it]);
     }
    close(int_socket);
+   close(fd);
+   close(fpga_lwfd);
 }  
 
 			  
@@ -234,7 +303,6 @@ int main(int argc, char* argv[])
   initSocket(argv[1]);
   pthread_t bufferProducer, bufferConsumer;
   int err;
-
 
   err = pthread_create(&bufferProducer, NULL, f_bufferProducer, NULL);
   if (err)
